282faf6cb47249776c19e4997795be78beb05789
joelohrdev <55007850+joelohrdev@users.noreply.github.com>
2022-11-04 22:10:59 -0500

    Front end set up

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,6 @@
+<div class="max-w-3xl mx-auto py-5">
+    <div class="flex justify-between ">
+        <h1>Coding Blog</h1>
+        <a class="hover:opacity-50 transition" href="/">Home</a>
+    </div>
+</div>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <p class="text-red-500">test</p>
+</x-layout>
