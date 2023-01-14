188895178507d8b123fbaa5e0bad2ca449a0f865
joelohrdev <55007850+joelohrdev@users.noreply.github.com>
2023-01-14 09:25:08 -0600

    Renamed Posts to Blog

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    @section('title') {{ __('Posts') }} @endsection
+    @section('title') {{ __('Blog') }} @endsection
     <div class="max-w-3xl mx-auto pt-14 pb-6 md:pb-12 md:pt-6 px-8 md:px-0 flex-1">
         <livewire:show-posts />
     </div>
--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,14 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', function () {
+    return view('index');
+});
 
 Route::get('/blog', function () {
     return view('blog.index');
 });
 
-Route::get('/posts/{post:slug}', [PostController::class, 'show'])->name('post.show');
+Route::get('/blog/{post:slug}', [PostController::class, 'show'])->name('post.show');
 
 Route::get('/category/{category:slug}', [CategoryController::class, 'show'])->name('category.show');
