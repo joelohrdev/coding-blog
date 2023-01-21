d9cac83a565599854bd9df31a1c472664f63bf80
joelohrdev <55007850+joelohrdev@users.noreply.github.com>
2023-01-21 09:44:20 -0600

    user

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,6 +45,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessFilament(): bool
     {
-        return str_ends_with($this->email, '@emailme.com');
+        return str_ends_with($this->email, '@emailme.com') && $this->hasVerifiedEmail();
     }
 }
